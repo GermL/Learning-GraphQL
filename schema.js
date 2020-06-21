9d498d971bcf05c5cf3a9060a73326881e7f38a1
germl <germy.louis@enhanceit.us>
2020-06-20 23:56:12 -0400

    Getting List of Books with titles and isbn number

--- schema.js
+++ schema.js
@@ -5,9 +5,27 @@ const{
     GraphQLSchema,
     GraphQLInt,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString, 
+    GraphQLList
 } = require('graphql')
 
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: '...',
+
+    fields: () => ({
+        title: {
+            type: GraphQLString,
+            resolve:
+            xml =>
+            xml.title[0]
+        },
+        isbn: {
+            type: GraphQLString,
+            resolve: xml => xml.isbn[0]
+        }
+    })
+})
 
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
@@ -18,6 +36,11 @@ const AuthorType = new GraphQLObjectType({
             type: GraphQLString,
             resolve: xml =>
             xml.GoodreadsResponse.author[0].name[0]
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: xml =>
+            xml.GoodreadsResponse.author[0].books[0].book
         }
     })
 })
